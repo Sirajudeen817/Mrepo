c3e6d3a051bb9491e53e661d21f5d4578eb02436
Sirajudeen817 <hameedz2003@gmail.com>
2025-11-28 09:02:11 +0530

    Add files via upload

---
+++ DesignPattern/HDProcessing.java
@@ -0,0 +1,12 @@
+package com.DesignPattern;
+
+public class HDProcessing implements ProcessingQuality{
+
+	@Override
+	public void processVideo() {
+		// TODO Auto-generated method stub
+		System.out.println(" HD quality");
+		
+	}
+
+}
